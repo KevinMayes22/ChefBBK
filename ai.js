18de20611eb28df8dfbbad3aecc755253412aa6e
KevinMayes22 <kevin@kmayes.co.uk>
2025-05-03 22:28:53 +0100

    More bug fixes

--- ai.js
+++ ai.js
@@ -1,8 +1,6 @@
 import Anthropic from "@anthropic-ai/sdk"
 
-const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. It's important ingredients measurements are metric and not imperial. Format your response in markdown to make it easier to render to a web page
-`
+
 const callAnthropic = async (prompt) => {
     const res = await fetch('/api/anthropic', {
       method: 'POST',
@@ -20,6 +18,6 @@ export async function getRecipeFromChefClaude(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
 
     msg = await callAnthropic(`I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`);
-    return msg.content[0].text
+    return msg;
 }
 
