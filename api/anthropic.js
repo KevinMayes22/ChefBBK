52e83d325919074934136e337c5c17e298a05e49
KevinMayes22 <kevin@kmayes.co.uk>
2025-05-03 22:24:46 +0100

    Fix bugs

--- api/anthropic.js
+++ api/anthropic.js
@@ -12,19 +12,23 @@ export default async function handler(req, res) {
     }
   
     try {
-      const response = await fetch('https://api.anthropic.com/v1/messages', {
+        const SYSTEM_PROMPT = `
+        You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. It's important ingredients measurements are metric and not imperial. Format your response in markdown to make it easier to render to a web page
+        `
+        
+        const response = await fetch('https://api.anthropic.com/v1/messages', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'x-api-key': process.env.ANTHROPIC_API_KEY,
-          'anthropic-version': '2023-06-01',
+          'anthropic-version': '2023-01-01',
         },
         body: JSON.stringify({
             model: "claude-3-haiku-20240307",
             max_tokens: 1024,
             system: SYSTEM_PROMPT,
             messages: [
-                { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
+                { role: "user", content: prompt  },
             ],
         }),
       });
